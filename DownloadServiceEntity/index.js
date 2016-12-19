86de56e55e584177d8ccaf5839ad1155d7cdd61d
steven hunt <st3v3n.hunt@gmail.com>
2016-12-19 15:59:59 +0000

    :ok_woman: Ensure done is called when things are done

--- DownloadServiceEntity/index.js
+++ DownloadServiceEntity/index.js
@@ -8,12 +8,12 @@ module.exports = function (context, myQueueItem) {
   request(url, (err, res, body) => {
     if (err) {
       context.log(`An error occurred during download of \n URL: ${url} \n ERROR: ${err}`);
+      context.done(err);
     } else if (res.statusCode === 200) {
       context.bindings.rawOrg = JSON.parse(body);
+      context.done();
     } else {
-      context.log(`Non 200 response encountered during download of \n URL: ${URL}`);
+      context.done(`Non 200 response encountered during download of \n URL: ${URL}`);
     }
   });
-
-  context.done();
 };
