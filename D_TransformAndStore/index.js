0e36fc89a4e20f13a568516bcc18442bb68ff121
steven hunt <st3v3n.hunt@gmail.com>
2016-12-21 17:35:16 +0000

    :no_bicycles: Record saved items in queue

--- D_TransformAndStore/index.js
+++ D_TransformAndStore/index.js
@@ -2,7 +2,9 @@ module.exports = function (context, rawOrg) {
   // Set the id so existing entries will be updated
   // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-documentdb#output-usage
   rawOrg.id = rawOrg.identifier;
-
   context.bindings.org = rawOrg;
+
+  // Send id of org to queue for record of what has been saved to docDB
+  context.bindings.orgId = rawOrg.id;
   context.done();
 };
