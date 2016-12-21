777a8fdad6a3a681b546747b51d830ee4feff347
steven hunt <st3v3n.hunt@gmail.com>
2016-12-21 14:31:52 +0000

    :wrench: Update docs if they already exist

--- D_TransformAndStore/index.js
+++ D_TransformAndStore/index.js
@@ -1,8 +1,7 @@
-module.exports = function (context, myQueueItem) {
-  const rawOrg = myQueueItem;
-
-  context.log('Placeholder for some data transformation.');
-  // TODO: Clean up/transform the data
+module.exports = function (context, rawOrg) {
+  // Set the id so existing entries will be updated
+  // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-documentdb#output-usage
+  rawOrg.id = rawOrg.identifier;
 
   context.bindings.org = rawOrg;
   context.done();
