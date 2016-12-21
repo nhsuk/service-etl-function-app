27fff5e7c97113c9f97bc1ead46866aaeb21c24f
steven hunt <st3v3n.hunt@gmail.com>
2016-12-21 08:30:38 +0000

    :door: Only Active pharmacies should be added

--- ODSFileUploadTrigger/index.js
+++ ODSFileUploadTrigger/index.js
@@ -1,17 +1,21 @@
 module.exports = function (context, odsDataFile) {
-    context.log('JavaScript blob trigger function processed blob \n Name:', context.bindingData.odaDataFile, '\n Blob Size:', odsDataFile.length, 'Bytes');
+  context.log('JavaScript blob trigger function processed blob \n Name:', context.bindingData.odaDataFile, '\n Blob Size:', odsDataFile.length, 'Bytes');
 
-    const odsCodes = [];
+  const odsCodes = [];
 
-    odsDataFile.split('\n').map((line) => {
-        odsCodes.push({
-            'odsCode': line.split(',')[0],
-            'orgType': 'pharmacy',
-            });
-    });
+  odsDataFile.split('\n').map((line) => {
+    const columns = line.split(',');
 
-    context.bindings.odsCodes = odsCodes;
-    context.log(`A total of: ${odsCodes.length} ods codes have been sent to the queue.`);
+    if (columns[12] === 'A') {
+      odsCodes.push({
+        'odsCode': columns[0],
+        'orgType': 'pharmacy',
+      });
+    }
+  });
 
-    context.done();
+  context.bindings.odsCodes = odsCodes;
+  context.log(`A total of: ${odsCodes.length} ods codes have been sent to the queue.`);
+
+  context.done();
 };
