5360d3970c9310f16bd6327404e1f0473eaa045e
steven hunt <st3v3n.hunt@gmail.com>
2016-12-22 16:24:43 +0000

    :cyclone: Attempt to prevent timeout by splitting processing

--- A_ProcessFile/index.js
+++ A_ProcessFile/index.js
@@ -9,7 +9,6 @@ module.exports = function (context, odsDataFile) {
   const halfWay = Math.round(lines.length / 2);
   let i;
 
-  context.log('halfWay: ' + halfWay);
   for (i = 0; i < lines.length; i++) {
     const columns = lines[i].split(',');
     const active = columns[12];
@@ -19,22 +18,20 @@ module.exports = function (context, odsDataFile) {
       const org = { odsCode: columns[0], orgType: 'pharmacy' };
 
       if (i < halfWay) {
-        context.log('First half' + org);
         odsCodesPartOne.push(org);
       } else {
-        context.log('Second half' + org);
         odsCodesPartTwo.push(org);
       }
     }
   }
 
   /* eslint-disable no-param-reassign */
-  context.bindings.odsCodesPartOne = odsCodesPartOne;
-  context.bindings.odsCodesPartTwo = odsCodesPartTwo;
+  context.bindings.odsCodesPartOne = JSON.stringify(odsCodesPartOne);
+  context.bindings.odsCodesPartTwo = JSON.stringify(odsCodesPartTwo);
   /* eslint-enable no-param-reassign */
 
   const totalItems = odsCodesPartOne.length + odsCodesPartTwo.length;
-  context.log(`A total of: ${totalItems} ods codes have been sent to the queue.`);
+  context.log(`A total of: ${totalItems} ods codes have been processed.`);
 
   context.done();
 };
