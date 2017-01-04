fdd525b668a79c48fef26636f7acea8c2727984a
steven hunt <st3v3n.hunt@gmail.com>
2017-01-04 09:28:08 +0000

    :triangular_ruler: Use CSV parser

--- A_SplitFile/index.js
+++ A_SplitFile/index.js
@@ -1,35 +1,43 @@
- // eslint-disable-next-line func-names
+const parse = require('csv-parse');
+
+// eslint-disable-next-line func-names
 module.exports = function (context, odsDataFile) {
   const odsCodesPartOne = [];
   const odsCodesPartTwo = [];
 
-  const lines = odsDataFile.split('\n');
-  const halfWay = Math.round(lines.length / 2);
-  let i;
+  parse(odsDataFile, (err, output) => {
+    if (err) {
+      context.done(err);
+    }
 
-  for (i = 0; i < lines.length; i++) {
-    const columns = lines[i].split(',');
-    const active = columns[12];
-    const orgSubType = columns[13];
+    const numberOfRecords = output.length;
+    const halfWay = Math.round(numberOfRecords / 2);
+    let i;
 
-    if (active === 'A' && orgSubType === '1') {
-      const org = { odsCode: columns[0], orgType: 'pharmacy' };
+    for (i = 0; i < numberOfRecords; i++) {
+      const record = output[i];
+      const active = record[12];
+      const orgSubType = record[13];
 
-      if (i < halfWay) {
-        odsCodesPartOne.push(org);
-      } else {
-        odsCodesPartTwo.push(org);
+      if (active === 'A' && orgSubType === '1') {
+        const org = { odsCode: record[0], orgType: 'pharmacy' };
+
+        if (i < halfWay) {
+          odsCodesPartOne.push(org);
+        } else {
+          odsCodesPartTwo.push(org);
+        }
       }
     }
-  }
 
-  /* eslint-disable no-param-reassign */
-  context.bindings.odsCodesPartOne = JSON.stringify(odsCodesPartOne);
-  context.bindings.odsCodesPartTwo = JSON.stringify(odsCodesPartTwo);
-  /* eslint-enable no-param-reassign */
+    /* eslint-disable no-param-reassign */
+    context.bindings.odsCodesPartOne = JSON.stringify(odsCodesPartOne);
+    context.bindings.odsCodesPartTwo = JSON.stringify(odsCodesPartTwo);
+    /* eslint-enable no-param-reassign */
 
-  const totalItems = odsCodesPartOne.length + odsCodesPartTwo.length;
-  context.log(`A total of: ${totalItems} ods codes have been processed.`);
+    const totalItems = odsCodesPartOne.length + odsCodesPartTwo.length;
+    context.log(`A total of: ${totalItems} ods codes have been processed.`);
 
-  context.done();
+    context.done();
+  });
 };
