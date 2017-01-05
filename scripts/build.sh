fed001262047d30474c323c57df077ee8ef32bc9
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-05 08:54:10 +0000

    :envelope: Pass github repo details to deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,13 @@ timeStamp=`date "+%Y%m%d%H%M%S"`
 baseName=`whoami`-dev-${functionName}-${timeStamp}
 resourceGroup=${baseName}
 region=uksouth
+currentRepoPath=`git rev-parse --show-toplevel`
+currentRepo=`basename $currentRepoPath`
+currentBranch=`git rev-parse --abbrev-ref HEAD`
+parameters="{ \"github_repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" } }"
 
 azure group create $resourceGroup $region
-azure group deployment create --resource-group $resourceGroup --template-file ./template.json #--parameters-file ./parameters.json
+azure group deployment create \
+  --resource-group $resourceGroup \
+  --template-file ./template.json \
+  --parameters "{ \"github_repo\": { \"value\": \"$currentRepo\" }, \"branch\": { \"value\": \"$currentBranch\" } }"
