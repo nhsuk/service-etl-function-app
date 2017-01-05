95f913cf4ad97d6265abbc53f22ea4afb6d3a5f0
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-05 14:19:41 +0000

    :art: Switch to use new az CLI
    Also, tag created resource group and resources

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,17 @@ baseName=`whoami`-dev-${functionName}-${timeStamp}
 resourceGroup=${baseName}
 region=uksouth
 currentRepoPath=`git rev-parse --show-toplevel`
-currentRepo=`basename $currentRepoPath`
+currentOrg=`git remote get-url origin | cut -f4 -d"/"`
+currentRepo=`git remote get-url origin | cut -f5 -d"/" | cut -f1 -d'.'`
 currentBranch=`git rev-parse --abbrev-ref HEAD`
-parameters="{ \"github_repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" } }"
 
-azure group create $resourceGroup $region
-azure group deployment create \
+echo "Creating infrastructure for ${currentOrg}/${currentRepo}/${currentBranch}" 
+
+az group create \
+  --name $resourceGroup \
+  --location $region \
+  --tags org=$currentOrg repo=$currentRepo branch=$currentBranch
+az group deployment create \
   --resource-group $resourceGroup \
   --template-file ./template.json \
-  --parameters "{ \"github_repo\": { \"value\": \"$currentRepo\" }, \"branch\": { \"value\": \"$currentBranch\" } }"
+  --parameters "{ \"org\": { \"value\": \"$currentOrg\" }, \"repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" } }"
