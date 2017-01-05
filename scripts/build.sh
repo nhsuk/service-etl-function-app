b801a9f9f1e4639a3f9ea0318b98cd6d15d22156
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-05 17:11:34 +0000

    :honey_pot: Create container in storage location

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,3 +18,14 @@ az group deployment create \
   --resource-group $resourceGroup \
   --template-file ./template.json \
   --parameters "{ \"org\": { \"value\": \"$currentOrg\" }, \"repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" } }"
+
+storageAccountName=`az storage account list --resource-group ${resourceGroup} --output list --query '[].name'`
+storageAccountConnectionString=`az storage account show-connection-string --resource-group ${resourceGroup} --name ${storageAccountName} --query 'connectionString'`
+
+# storageAccountKey=`az storage account keys list --resource-group ${resourceGroup} --name ${storageAccountName} --query 'keys[0].value'`
+# This through an AccountName error - not sure why
+# az storage container create --name ods-input --account-name $storageAccountName --account-key=$storageAccountKey
+
+AZURE_STORAGE_CONNECTION_STRING=${storageAccountConnectionString} az storage container create --name ods-input
+
+
