4d6184f0e80f182f518743dc97de391f46d83101
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-06 17:47:49 +0000

    :fallen_leaf: Us a common dev plan
    Server farms (aka plans) cost so use a common one rather than one per
    instance of the function app.
    
    This is an experimental branch and may be discarded.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,8 @@ region=uksouth
 currentRepoPath=`git rev-parse --show-toplevel`
 currentOrg=`git remote get-url origin | cut -f4 -d"/"`
 currentRepo=`git remote get-url origin | cut -f5 -d"/" | cut -f1 -d'.'`
-currentBranch=`git rev-parse --abbrev-ref HEAD`
+# currentBranch=`git rev-parse --abbrev-ref HEAD`
+currentBranch=master
 
 echo "Creating infrastructure for ${currentOrg}/${currentRepo}/${currentBranch}" 
 
@@ -14,11 +15,20 @@ az group create \
   --name $resourceGroup \
   --location $region \
   --tags org=$currentOrg repo=$currentRepo branch=$currentBranch
+
+if [ `az resource list --resource-group c2s-common-dev --name c2s-plan-dev --query '[].id' | wc -l ` == "0" ]
+then
+  az group deployment create \
+    --name "scripted-deployment-${timeStamp}" \
+    --resource-group c2s-common-dev \
+    --template-file ./dev-common-template.json
+fi
+
 az group deployment create \
   --name "scripted-deployment-${timeStamp}" \
   --resource-group $resourceGroup \
   --template-file ./template.json \
-  --parameters "{ \"org\": { \"value\": \"$currentOrg\" }, \"repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" } }"
+  --parameters "{ \"org\": { \"value\": \"$currentOrg\" }, \"repo\": { \"value\": \"$currentRepo\" } , \"branch\": { \"value\": \"$currentBranch\" }, \"plan-resource-group\": { \"value\": \"c2s-common-dev\" }, \"plan-name\" : { \"value\": \"c2s-plan-dev\" }  }"
 
 storageAccountName=`az storage account list --resource-group ${resourceGroup} --output list --query '[].name'`
 storageAccountConnectionString=`az storage account show-connection-string --resource-group ${resourceGroup} --name ${storageAccountName} --query 'connectionString'`
